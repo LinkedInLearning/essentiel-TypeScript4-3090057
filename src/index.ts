f5645fcfd7aaddb667b95d3fd47fea19cb8228c8
ElisePatrikainen <elise.patrikainen@gmail.com>
2022-10-11 18:55:34 +0200

    chore: add typescript configuration on node environment

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,3 @@ let uneVariableDeTypeString: number | string = "hello"
 uneVariableDeTypeString = 2
 console.log(uneVariableDeTypeString)
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div>${uneVariableDeTypeString}</div>
-`
